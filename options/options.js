2462f9685babc962ed22dcd72a4db3e356175e32
Crowdbotics <team@crowdbotics.com>
2021-08-19 21:01:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "phila_language_29955Identifier",
+  url: "https://phila_language_29955Identifier.botics.co",
+  api: "https://phila_language_29955Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
